f8a21ab658ae91bc3d02e9abb3ff5b4d85c5bd75
Jose Badillo <utm20040072@utma.edu.mx>
2022-07-07 09:45:51 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { CrudComponent } from './pages/crud/crud.component';
+import { ActualizarUsuarioComponent } from './pages/crud/actualizar-usuario/actualizar-usuario.component';
+import { RegistrarUsuarioComponent } from './pages/crud/registrar-usuario/registrar-usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CrudComponent,
+    ActualizarUsuarioComponent,
+    RegistrarUsuarioComponent
   ],
   imports: [
     BrowserModule,
